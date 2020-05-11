0eeb9df05270984d9e79ed2c29968c0e63ad911d
Dongin Kim <doinki94@gmail.com>
2020-05-11 23:26:22 +0900

    add: schema

---
+++ models/user.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      username: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+    },
+    {
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    },
+  );
+
+  User.associate = (db) => {
+    db.User.hasMany(db.Post, { as: 'Posts' });
+    db.User.hasMany(db.Comment);
+    db.User.belongsToMany(db.Post, { through: 'Like', as: 'Liked' });
+    db.User.belongsToMany(db.User, {
+      through: 'Follow',
+      as: 'Followers',
+      foreignKey: 'followingId',
+    });
+    db.User.belongsToMany(db.User, {
+      through: 'Follow',
+      as: 'Followings',
+      foreignKey: 'followerId',
+    });
+  };
+
+  return User;
+};
